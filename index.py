88e015005f82183d2d3c0b44aa39779f5e60c53d
ferdyaqliyansyah <ferdyaqli27@gmail.com>
2026-09-16 15:53:02 +0700

    refactor: adjust count_price_after_discount function and add count_total_price function

--- index.py
+++ index.py
@@ -1,7 +1,15 @@
-def count_price_after_discount(unit_price, quantity, total_items):
+def count_total_price(unit_price, quantity, is_member = False, delivery_fee = 7000):
+  
+  total_price = count_price_after_discount(unit_price, quantity)
+
+  total_price = count_delivery_fee(total_price, is_member, delivery_fee)
+    
+  return total_price
+
+def count_price_after_discount(unit_price, quantity):
     subtotal = unit_price * quantity
 
-    if total_items > 100:
+    if quantity > 100:
         discount_rate = 0.10
     else:
         discount_rate = 0.05
@@ -14,3 +22,11 @@ def count_delivery_fee(total_price, is_member, delivery_fee):
     if not is_member:
         return total_price + delivery_fee
     return total_price
+
+# Nama yang penting adalah total_price yang mana merupakan total yang harus 
+# dibayar oleh pembeli, termasuk count_price_after_discount dan count_delivery_fee 
+# jika berlaku. Fungsi ini menghitung total harga berdasarkan harga per item, jumlah item, 
+# status keanggotaan, dan biaya pengiriman.
+  
+
+print(count_total_price(1000, 200))
